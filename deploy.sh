6baccd3f72c7abe56985f7bd45b8f9e601ffc2bf
sebatyler <sebatyler@gmail.com>
2022-07-13 18:22:31 +0900

    Test

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ flutter build web --release --web-renderer canvaskit
 echo "dist_id: $WEB_CLOUDFRONT_DIST_ID"
 echo "aws key: $AWS_ACCESS_KEY_ID"
 echo "aws secret key: $AWS_ACCESS_KEY_ID"
-aws sts get-caller-identity
+aws s3 ls
 
 # check if logged in
 if aws sts get-caller-identity && [[ $WEB_CLOUDFRONT_DIST_ID != "" ]]
