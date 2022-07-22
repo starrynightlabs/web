15cb73d65d04a635b0b975e8924b97b6ce053bfb
sebatyler <sebatyler@gmail.com>
2022-07-22 12:04:47 +0900

    Deploy to nyxs.io

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ flutter build web --release --web-renderer canvaskit
 if aws sts get-caller-identity > /dev/null && [[ $WEB_CLOUDFRONT_DIST_ID != "" ]]
 then
   # sync
-  aws s3 sync build/web/ s3://starrynight-web --delete
+  aws s3 sync build/web/ s3://nyxs-web --delete
 
   # starrynight web by flutter
   aws cloudfront create-invalidation --distribution-id "$WEB_CLOUDFRONT_DIST_ID" --paths '/*'
